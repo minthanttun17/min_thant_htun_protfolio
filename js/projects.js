afd5174cf03342bba937107d7921103747b458f0
Min Thant Htun <minthanthtun15907@gmail.com>
2023-01-06 19:10:25 +0630

    added projects & changed

--- js/projects.js
+++ js/projects.js
@@ -4,5 +4,23 @@ projects = [
         "github": "https://github.com/minthanthtun/uni_project",
         "demon": "https://minthanthtun.github.io/uni_project/",
         "img": "./img/wiring.jpg"
+    },
+    {
+        "name": "to do website",
+        "github": "https://github.com/minthanthtun/todo_list",
+        "demon": "https://minthanthtun.github.io/todo_list/",
+        "img": "./img/todo.png"
+    },
+    {
+        "name": "the weeknd playlist",
+        "github": "https://github.com/minthanthtun/the_weeknd_playlist",
+        "demon": "https://minthanthtun.github.io/the_weeknd_playlist/",
+        "img": "./img/the_weeknd_playlist.png"
+    },
+    {
+        "name": "stop watch",
+        "github": "https://github.com/minthanthtun/stop_watch",
+        "demon": "https://minthanthtun.github.io/stop_watch/",
+        "img": "./img/stop_watch.png"
     }
 ]
